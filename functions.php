f07edf8c119f834785bf881db2b09721c108f1f9
sezj <sarahzachrich@gmail.com>
2019-06-28 16:15:01 -0400

    Define child theme includes directory; Define ACF Local JSON load/save directory; add sample field groups

--- functions.php
+++ functions.php
@@ -37,3 +37,25 @@ if ( !function_exists( 'hwcoe_ufl_child_icon_url' ) ) {
 	}
 	add_filter( 'get_site_icon_url', 'hwcoe_ufl_child_icon_url' );
 }
+
+/*
+ * Theme variable definitions
+ */
+define( "HWCOE_UFL_CHILD_INC_DIR", get_stylesheet_directory() . "/inc/modules" );
+
+// Integrate Advanced Custom Fields
+add_filter( 'acf/settings/save_json', function() {
+	// save to the acf-json directory in child theme folder
+    return get_stylesheet_directory() . '/inc/acf-json';
+} );
+
+add_filter( 'acf/settings/load_json', function( $paths ) {
+    unset( $paths[0] );
+    
+    // load child theme custom fields
+    $paths[] = get_stylesheet_directory() . '/inc/acf-json';
+    // load parent theme custom fields
+    $paths[] = get_template_directory() . '/inc/advanced-custom-fields/acf-json/';
+
+    return $paths;
+} );
--- readme.md
+++ readme.md
@@ -7,6 +7,10 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 ## Changelog
 
+1.1.0-beta
+- Define child theme includes directory
+- Define ACF Local JSON load/save directory; add sample field groups
+
 1.0.0-beta
 - Enqueue dependencies, parent theme and child theme styles
 - Enqueue example child scripts
