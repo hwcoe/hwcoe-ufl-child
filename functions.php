467a107577e3a962322fbf1babd72fcba0a58c07
sezj <sarahzachrich@gmail.com>
2019-07-05 11:35:20 -0400

    Remove Career Fair child theme functions and change definition of HWCOE_UFL_CHILD_INC_DIR constant

--- functions.php
+++ functions.php
@@ -21,8 +21,6 @@ function hwcoe_ufl_child_scripts() {
 		get_theme_version() 
 	);
 	
-	wp_enqueue_script('mixitup', get_stylesheet_directory_uri() . '/js/mixitup.min.js', array(), get_theme_version(), true);
-	
 	wp_enqueue_script('hwcoe-ufl-child-scripts', get_stylesheet_directory_uri() . '/scripts.js', array(), get_theme_version(), true);
 		
 }
@@ -43,76 +41,14 @@ if ( !function_exists( 'hwcoe_ufl_child_icon_url' ) ) {
 /*
  * Theme variable definitions
  */
-define( "HWCOE_UFL_CAREER_INC_DIR", get_stylesheet_directory() . "/inc/modules" );
-/*
-* Visual Editor Styles
-*/
+define( "HWCOE_UFL_CHILD_INC_DIR", get_stylesheet_directory() . "/inc" );
 
-function my_mce_buttons_2($buttons) {
-    array_unshift($buttons, 'styleselect');
-    return $buttons;
-}
-add_filter('mce_buttons_2', 'my_mce_buttons_2');
-
-// Callback function to filter the MCE settings
-function my_mce_before_init_insert_formats( $init_array ) {  
-	// Define the style_formats array
-	$style_formats = array(  
-		// Each array child is a format with it's own settings
-		array(  
-			'title' => 'Button',  
-			'block' => 'div',  
-			'classes' => 'cf-single-button',
-			'wrapper' => true,
-			
-		),  
-	);  
-	// Insert the array, JSON ENCODED, into 'style_formats'
-	$init_array['style_formats'] = json_encode( $style_formats );  
-	
-	return $init_array;  
-  
-} 
-// Attach callback to 'tiny_mce_before_init' 
-add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );  
-
-function create_post_type() {
-  register_post_type( 'cf-event_information',
-    array(
-      'labels' => array(
-        'name' => __( 'Details' ), //Top of page when in post type
-        'singular_name' => __( 'Event Information' ), //per post
-		'menu_name' => __('Event Information'), //Shows up on side menu
-		'all_items' => __('Event Details'), //On side menu as name of all items
-	  ),
-      'public' => true,
-	  'menu_position' => 4,
-      'has_archive' => true,
-    )
-  );
-}
-add_action( 'init', 'create_post_type' );
-
-
-// Integrate Advanced Custom Fields
-// add_filter( 'acf/settings/save_json', function() {
-// 	// save to the acf-json directory in child theme folder
-//     return get_stylesheet_directory() . '/inc/acf-json';
-// } );
-
-// add_filter( 'acf/settings/load_json', function( $paths ) {
-//     unset( $paths[0] );
-    
-//     // load child theme custom fields
-//     $paths[] = get_stylesheet_directory() . '/inc/acf-json';
-//     // load parent theme custom fields
-//     $paths[] = get_template_directory() . '/inc/advanced-custom-fields/acf-json';
-
-//     return $paths;
-// } );
 
 // Integrating Advanced Custom Fields
-// remove_filter( 'acf/settings/save_json', 'hwcoe_ufl_acf_json_save_point' );
+remove_filter( 'acf/settings/save_json', 'hwcoe_ufl_acf_json_save_point' );
+add_filter( 'acf/settings/save_json', function() {
+    return HWCOE_UFL_CHILD_INC_DIR . '/acf-json';
+} );
 
 // add_filter( 'acf/settings/save_json', function( $path ) {
 // 	unset($path);
@@ -133,14 +69,23 @@ add_action( 'init', 'create_post_type' );
 // 	return $path;
 // }
 
-// remove_filter('acf/settings/load_json', 'hwcoe_ufl_acf_json_load_point');
-
+remove_filter('acf/settings/load_json', 'hwcoe_ufl_acf_json_load_point');
 add_filter( 'acf/settings/load_json', function( $paths ) {
 	unset($paths[0]);
-    array_push(
-        $paths,
-        get_stylesheet_directory() . '/inc/acf-json',
-        get_template_directory() . '/inc/advanced-custom-fields/acf-json'
-    );
-    return $paths;
+   
+	array_push(
+		$paths,
+		// load child theme custom fields
+		HWCOE_UFL_CHILD_INC_DIR . '/acf-json',
+		// load parent theme custom fields		
+		get_template_directory() . '/inc/advanced-custom-fields/acf-json'
+	);
+
+	return $paths;
 } );
+
+// Integrate Advanced Custom Fields
+// add_filter( 'acf/settings/save_json', function() {
+// 	// save to the acf-json directory in child theme folder
+//     return get_stylesheet_directory() . '/inc/acf-json';
+// } );
